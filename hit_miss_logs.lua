866939514303443b29a0aa6ab23359ffd490c386
Aimware0 <75004732+Aimware0@users.noreply.github.com>
2021-02-09 21:32:18 -0500

    Update hit_miss_logs.lua

--- hit_miss_logs.lua
+++ hit_miss_logs.lua
@@ -1,60 +1,18 @@
+local notify_lib_installed = false
 
-local notify = {
-	notfications = {},
-	prefix = '',
-	PrefixColor = {255,0,0},
-	MessageColor = {255,255,255},
-	margin = 10,
-
-	SetPrefix = function(self, prefix) self.prefix = prefix end,
-	SetPrefixColor = function(self, color) self.PrefixColor = color end,
-	SetMessageColor = function(self, color)	self.MessageColor = color end,
-	SetMargin = function(self, margin) self.margin = margin end,
-
-	SetFont = function(self, font) self.font = font end,
-
-	Add = function(self, message, duration, shouldPrint)
-		if shouldPrint then
-			print(self.prefix .. " " ..  message)
-		end
-		table.insert(self.notfications, {
-			prefix = self.prefix,
-			message = message,
-			duration = duration,
-			PrefixColor = self.PrefixColor,
-			MessageColor = self.MessageColor,
-			font = self.font,
-			init_time = globals.CurTime(),
-			margin = self.margin
-		})
-	end,
-}
-
-callbacks.Register("Draw", function()
-	if not entities.GetLocalPlayer() then notify.notfications = {} end
-	for i, notfication in ipairs(notify.notfications) do
-		draw.SetFont(notfication.font)
-		local prefix_x, prefix_y = draw.GetTextSize(notfication.prefix .. " ")
-		local message_x, message_y = "", ""
-		if not notify.notfications[i - 1] then
-			message_x, message_y = draw.GetTextSize(notfication.message) -- this should really used the last element's message instead of it's own; don't know how to gracefully do it in lua
-		else
-			message_x, message_y = draw.GetTextSize(notify.notfications[i - 1].message)
-		end
-		draw.Color(notfication.PrefixColor[1], notfication.PrefixColor[2], notfication.PrefixColor[3])
-		draw.Text(0, (message_y + notfication.margin) * (i - 1), notfication.prefix)
-
-		draw.Color(notfication.MessageColor[1], notfication.MessageColor[2], notfication.MessageColor[3])
-		draw.Text(prefix_x, (message_y + notfication.margin) * (i - 1), notfication.message)
-	
-
-		if globals.CurTime() >= notfication.init_time + notfication.duration then
-			table.remove(notify.notfications, i)
-		end
+file.Enumerate(function(filename)
+	if filename == "libraries/chickens_notfication.lua" then
+		notify_lib_installed = true
 	end
 end)
 
--- End of notifcation library
+if not notify_lib_installed then
+	local body = http.Get("https://raw.githubusercontent.com/Aimware0/aimware_scripts/main/libraries/chickens_notfication.lua")
+	file.Write("libraries/chickens_notfication.lua", body)
+end
+
+RunScript("libraries/chickens_notfication.lua")
+
 
 local hitboxes = {
     'head',
